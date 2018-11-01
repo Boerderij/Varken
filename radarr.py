158f4f3eaf46f4ee4fe0e3cb41c727ec1d62fcb5
samwiseg0 <samwiseg00@gmail.com>
2018-11-01 16:03:03 -0400

    Clean up some formatting

--- radarr.py
+++ radarr.py
@@ -14,8 +14,12 @@ def now_iso():
 
 
 def influx_sender(influx_payload):
-    influx = InfluxDBClient(configuration.influxdb_url, configuration.influxdb_port, configuration.influxdb_username,
-                            configuration.influxdb_password, configuration.radarr_influxdb_db_name)
+    influx = InfluxDBClient(configuration.influxdb_url,
+                            configuration.influxdb_port,
+                            configuration.influxdb_username,
+                            configuration.influxdb_password,
+                            configuration.radarr_influxdb_db_name)
+
     influx.write_points(influx_payload)
 
 
@@ -36,7 +40,8 @@ def get_missing_movies():
 
         for movie in movies.keys():
             if not movies[movie]['downloaded']:
-                movie_name = ('{} ({})'.format(movies[movie]['title'], movies[movie]['year']))
+                movie_name = ('{} ({})'.format(movies[movie]['title'],
+                                               movies[movie]['year']))
                 missing.append((movie_name, movies[movie]['tmdbId']))
 
         for movie, id in missing:
@@ -78,9 +83,9 @@ def get_missing_avl():
         for movie in movies.keys():
             if not movies[movie]['downloaded']:
                 if movies[movie]['isAvailable'] is True:
-                    movie_name = ('{} ({})'.format(movies[movie]['title'], movies[movie]['year']))
+                    movie_name = ('{} ({})'.format(movies[movie]['title'],
+                                                   movies[movie]['year']))
                     missing.append((movie_name, movies[movie]['tmdbId']))
-
 
         for movie, id in missing:
             influx_payload.append(
@@ -118,8 +123,11 @@ def get_queue_movies():
         queue_movies = {d['id']: d for d in get_movies}
 
         for movie in queue_movies.keys():
-            name = '{} ({})'.format(queue_movies[movie]['movie']['title'], queue_movies[movie]['movie']['year'])
+            name = '{} ({})'.format(queue_movies[movie]['movie']['title'],
+                                    queue_movies[movie]['movie']['year'])
+
             quality = (queue_movies[movie]['quality']['quality']['name'])
+
             protocol = (queue_movies[movie]['protocol'].upper())
 
             if protocol == 'USENET':
@@ -155,16 +163,17 @@ def get_queue_movies():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Radarr stats operations',
-        description='Script to aid in data gathering from Radarr', formatter_class=RawTextHelpFormatter)
+                                     description='Script to aid in data gathering from Radarr',
+                                     formatter_class=RawTextHelpFormatter)
 
-    parser.add_argument("--missing",  action='store_true',
-        help='Get missing movies')
+    parser.add_argument("--missing", action='store_true',
+                        help='Get missing movies')
 
-    parser.add_argument("--missing_avl",  action='store_true',
-        help='Get missing yet available movies')
+    parser.add_argument("--missing_avl", action='store_true',
+                        help='Get missing yet available movies')
 
-    parser.add_argument("--queue",  action='store_true',
-        help='Get movies in queue')
+    parser.add_argument("--queue", action='store_true',
+                        help='Get movies in queue')
 
     opts = parser.parse_args()
 
