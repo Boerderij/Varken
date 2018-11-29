c7c7e34bfef9273f674e29aee74f2247b3272070
Nicholas St. Germain <nick@cajun.pro>
2018-11-28 21:20:32 -0600

    added default values to helper classes

--- Varken/helpers.py
+++ Varken/helpers.py
@@ -2,41 +2,56 @@ from typing import NamedTuple
 
 
 class TVShow(NamedTuple):
-    seriesId: int
-    episodeFileId: int
-    seasonNumber: int
-    episodeNumber: int
-    title: str
-    airDate: str
-    airDateUtc: str
-    overview: str
-    episodeFile: dict
-    hasFile: bool
-    monitored: bool
-    unverifiedSceneNumbering: bool
-    absoluteEpisodeNumber: int
-    series: dict
-    id: int
+    seriesId: int = None
+    episodeFileId: int = None
+    seasonNumber: int = None
+    episodeNumber: int = None
+    title: str = None
+    airDate: str = None
+    airDateUtc: str = None
+    overview: str = None
+    episodeFile: dict = None
+    hasFile: bool = None
+    monitored: bool = None
+    unverifiedSceneNumbering: bool = None
+    absoluteEpisodeNumber: int = None
+    series: dict = None
+    id: int = None
 
 
 class Queue(NamedTuple):
-    series: dict
-    episode: dict
-    quality: dict
-    size: float
-    title: str
-    sizeleft: float
-    timeleft: str
-    estimatedCompletionTime: str
-    status: str
-    trackedDownloadStatus: str
-    statusMessages: list
-    downloadId: str
-    protocol: str
-    id: int
+    series: dict = None
+    episode: dict = None
+    quality: dict = None
+    size: float = None
+    title: str = None
+    sizeleft: float = None
+    timeleft: str = None
+    estimatedCompletionTime: str = None
+    status: str = None
+    trackedDownloadStatus: str = None
+    statusMessages: list = None
+    downloadId: str = None
+    protocol: str = None
+    id: int = None
 
 
 class Server(NamedTuple):
-    url: str
-    api_key: str
-    id: int
+    id: int = None
+    url: str = None
+    api_key: str = None
+    verify_ssl: bool = False
+
+
+class TautulliServer(NamedTuple):
+    url: str = None
+    fallback_ip: str = None
+    apikey: str = None
+    verify_ssl: bool = None
+    influx_db: str = None
+
+class InfluxServer(NamedTuple):
+    url: str = 'localhost'
+    port: int = 8086
+    username: str = 'root'
+    password: str = 'root'
