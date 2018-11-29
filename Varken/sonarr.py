dbece1968d9ecaaebb4cf4261835747f1ac310b3
dirtycajunrice <nicholas.totsch@gmail.com>
2018-11-28 22:20:42 -0600

    migrated sonarr.py to new style

--- Varken/sonarr.py
+++ Varken/sonarr.py
@@ -7,37 +7,21 @@ from influxdb import InfluxDBClient
 from datetime import datetime, timezone, date, timedelta
 
 from Varken import configuration as config
-from Varken.helpers import Server, TVShow, Queue
+from Varken.helpers import TVShow, Queue
 
 
 class SonarrAPI(object):
-    # Sets None as default for all TVShow NamedTuples, because sonarr's response json is inconsistent
-    TVShow.__new__.__defaults__ = (None,) * len(TVShow._fields)
-
-    def __init__(self):
+    def __init__(self, servers):
         # Set Time of initialization
         self.now = datetime.now(timezone.utc).astimezone().isoformat()
         self.today = str(date.today())
         self.influx = InfluxDBClient(config.influxdb_url, config.influxdb_port, config.influxdb_username,
                                      config.influxdb_password, config.sonarr_influxdb_db_name)
         self.influx_payload = []
-        self.servers = self.get_servers()
+        self.servers = servers
         # Create session to reduce server web thread load, and globally define pageSize for all requests
         self.session = requests.Session()
         self.session.params = {'pageSize': 1000}
-
-    @staticmethod
-    def get_servers():
-        # Ensure sonarr servers have been defined
-        if not config.sonarr_server_list:
-            sys.exit("No Sonarr servers defined in config")
-
-        # Build Server Objects from config
-        servers = []
-        for url, api_key, server_id in config.sonarr_server_list:
-            servers.append(Server(url=url, api_key=api_key, id=server_id))
-
-        return servers
 
     def get_missing(self, days_past):
         endpoint = '/api/calendar'
@@ -64,39 +48,6 @@ class SonarrAPI(object):
                         "measurement": "Sonarr",
                         "tags": {
                             "type": "Missing",
-                            "sonarrId": sonarr_id,
-                            "server": server.id
-                        },
-                        "time": self.now,
-                        "fields": {
-                            "name": series_title,
-                            "epname": episode_title,
-                            "sxe": sxe,
-                            "airs": air_date
-                        }
-                    }
-                )
-
-    def get_upcoming(self):
-        endpoint = '/api/calendar/'
-
-        for server in self.servers:
-            upcoming = []
-            headers = {'X-Api-Key': server.api_key}
-
-            get = self.session.get(server.url + endpoint, headers=headers).json()
-            tv_shows = [TVShow(**show) for show in get]
-
-            for show in tv_shows:
-                sxe = 'S{:0>2}E{:0>2}'.format(show.seasonNumber, show.episodeNumber)
-                upcoming.append((show.series['title'], sxe, show.id, show.title, show.airDate))
-
-            for series_title, sxe, sonarr_id, episode_title, air_date in upcoming:
-                self.influx_payload.append(
-                    {
-                        "measurement": "Sonarr",
-                        "tags": {
-                            "type": "Soon",
                             "sonarrId": sonarr_id,
                             "server": server.id
                         },
@@ -204,7 +155,6 @@ if __name__ == "__main__":
                         help='legacy command. Deprecated in favor of --missing'
                         '\nfunctions identically to --missing'
                         '\nNote: Will be removed in a future release')
-    parser.add_argument("--upcoming", action='store_true', help='Get upcoming TV shows')
     parser.add_argument("--future", metavar='$days', type=int,
                         help='Get TV shows on X days into the future. Includes today.'
                         '\ni.e. --future 2 is Today and Tomorrow')
