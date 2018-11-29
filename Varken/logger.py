62520eae6d758fccd891e7a0158005bd744ae5ac
dirtycajunrice <nicholas.totsch@gmail.com>
2018-11-28 22:57:10 -0600

    bunch of tweaks. Scheduling is working

--- Varken/logger.py
+++ Varken/logger.py
@@ -1,1 +1,11 @@
 import functools
+
+def logging(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        print('LOG: Running job "%s"' % function.__name__)
+        result = function(*args, **kwargs)
+        print('LOG: Job "%s" completed' % function.__name__)
+        return result
+
+    return wrapper
