57616c3870a6c8c4e9a6b8a9353a9875001f6755
dirtycajunrice <nicholas.totsch@gmail.com>
2018-12-03 23:47:46 -0600

    added ability to define data folder

--- varken.py
+++ varken.py
@@ -1,6 +1,10 @@
 import schedule
 import threading
+from sys import exit
 from time import sleep
+from os import access, R_OK
+from os.path import isdir, abspath, dirname, join
+from argparse import ArgumentParser, RawTextHelpFormatter
 
 from Varken.iniparser import INIParser
 from Varken.sonarr import SonarrAPI
@@ -15,7 +19,29 @@ def threaded(job):
 
 
 if __name__ == "__main__":
-    CONFIG = INIParser()
+    parser = ArgumentParser(prog='Varken',
+                            description='Command-line utility to aggregate data from the plex ecosystem into InfluxDB',
+                            formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument("-d", "--data-folder", help='Define an alternate data folder location')
+    parser.add_argument("-l", "--log-level", choices=['info', 'error', 'debug'], help='Not yet implemented')
+
+    opts = parser.parse_args()
+
+    DATA_FOLDER = abspath(join(dirname(__file__), 'data'))
+
+    if opts.data_folder:
+        ARG_FOLDER = opts.data_folder
+
+        if isdir(ARG_FOLDER):
+            DATA_FOLDER = ARG_FOLDER
+            if not access(ARG_FOLDER, R_OK):
+                exit("Read permission error for {}".format(ARG_FOLDER))
+        else:
+            exit("{} does not exist".format(ARG_FOLDER))
+
+
+    CONFIG = INIParser(DATA_FOLDER)
     DBMANAGER = DBManager(CONFIG.influx_server)
 
     if CONFIG.sonarr_enabled:
