d710d2c6212b671b2c9f53279ddb55e6b0acb7f3
samwiseg0 <samwiseg00@gmail.com>
2019-03-11 20:13:50 -0400

    Make changes to USG to prevent canceling job. Should fix #107

--- varken/unifi.py
+++ varken/unifi.py
@@ -37,8 +37,8 @@ class UniFiAPI(object):
         get = connection_handler(self.session, req, self.server.verify_ssl)
 
         if not get:
-            self.logger.error("Canceling Job get_usg_stats for unifi-%s", self.server.id)
-            return f"unifi-{self.server.id}-get_usg_stats"
+            self.logger.error("Disregarding Job get_usg_stats for unifi-%s", self.server.id)
+            return
 
         devices = {device['name']: device for device in get['data']}
         if devices.get(self.server.usg_name):
