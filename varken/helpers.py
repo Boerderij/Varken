3c9cd199de66ef92b22a5bebf89c7cd49d5d237b
samwiseg0 <phernandez@ReverseFlash.local>
2019-12-31 15:54:33 -0500

    Update formatting.

--- varken/helpers.py
+++ varken/helpers.py
@@ -28,7 +28,6 @@ class GeoIPHandler(object):
 
         self.logger.info('Opening persistent connection to the MaxMind DB...')
 
-
     def reader_manager(self, action=None):
         if action == 'open':
             try:
@@ -78,8 +77,9 @@ class GeoIPHandler(object):
 
     def download(self):
         tar_dbfile = abspath(join(self.data_folder, 'GeoLite2-City.tar.gz'))
-        maxmind_url = 'https://download.maxmind.com/app/geoip_download?edition_id={db}&suffix={suffix}&license_key={key}'.format(
-            db='GeoLite2-City', suffix='tar.gz', key=self.maxmind_license_key)
+        maxmind_url = ('https://download.maxmind.com/app/geoip_download?edition_id={db}&'
+        'suffix={suffix}&license_key={key}').format(db='GeoLite2-City', suffix='tar.gz',
+                                                    key=self.maxmind_license_key)
         downloaded = False
 
         retry_counter = 0
