16a1e83fe915492c0693c33676f99cb834f1797e
Nicholas St. Germain <nstgermain@ea.com>
2020-01-08 14:44:53 -0600

    add datamanager object with influx/prometheus sub-objects. WIP!

--- varken/datamanager.py
+++ varken/datamanager.py
@@ -3,9 +3,32 @@ from logging import getLogger
 from influxdb import InfluxDBClient
 from requests.exceptions import ConnectionError
 from influxdb.exceptions import InfluxDBServerError
+from prometheus_client import start_http_server, Counter, Gauge
 
 
-class DBManager(object):
+class DataManager(object):
+    def __init__(self, config):
+        self.config = config
+        self.logger = getLogger()
+        self.type = None
+        self.prometheus = None
+        self.influx = None
+        if type(self.config).__name__ == "PrometheusClient":
+            self.prometheus = PrometheusExporter(self.config)
+        else:
+            self.influx = InfluxClient(config)
+
+    def update(self, data):
+        json_data = data
+        if self.influx:
+            self.influx.write_points(data)
+        elif self.prometheus:
+            self.prometheus.parse_data(data)
+        else:
+            self.logger.error("Wtf... let a dev know a wtf happened...")
+
+
+class InfluxClient(object):
     def __init__(self, server):
         self.server = server
         self.logger = getLogger()
@@ -43,3 +66,13 @@ class DBManager(object):
         except (InfluxDBServerError, ConnectionError) as e:
             self.logger.error('Error writing data to influxdb. Dropping this set of data. '
                               'Check your database! Error: %s', e)
+
+
+class PrometheusExporter(object):
+    def __init__(self, config):
+        self.config = config
+        self.http_server = start_http_server(self.config.port, addr=self.config.url)
+        self.logger = getLogger()
+
+    def parse_data(self, data):
+        pass
