ddf80f115f576554c682cf3945da7ffc64e178fa
Stewart Thomson <stewartthomson3@gmail.com>
2022-01-17 22:06:49 -0500

    Update lidarr structure
    Added missing arguments to Lidarr structure
    
    Fixes #223

--- varken/structures.py
+++ varken/structures.py
@@ -608,7 +608,9 @@ class LidarrQueue(NamedTuple):
     sizeleft: float = None
     status: str = None
     trackedDownloadStatus: str = None
+    trackedDownloadState: str = None
     statusMessages: list = None
+    errorMessage: str = None
     downloadId: str = None
     protocol: str = None
     downloadClient: str = None
